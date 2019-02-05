eec2ae8a99035253a560f818f86882f096eb43b6
benjrome <benjrome@gmail.com>
2019-02-05 09:53:51 +0000

    more deploymetn fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'beerme'`, () => {
+  it(`should have as title 'beer-my-food'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('beerme');
+    expect(app.title).toEqual('beer-my-food');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to beerme!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to beer-my-food!');
   });
 });
